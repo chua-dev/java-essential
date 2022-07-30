43881a71eeb79d0adb1b627d5434d80af394527a
dev-chua <chua3940@gmail.com>
2022-07-30 16:38:48 +0800

    four different type of for loop for arraylist

--- src/com/arrayandlist/Main.java
+++ src/com/arrayandlist/Main.java
@@ -2,6 +2,7 @@ package com.arrayandlist;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
@@ -29,5 +30,43 @@ public class Main {
         // Set An Element At Index I
         friendsArray2[0] = "Carl";
         friendsArrayList2.set(0, "Carl");
+
+        // Remove An Element
+        // Cannot do it with ARRAY
+        friendsArrayList2.remove("Chris");
+        friendsArrayList2.remove(1);
+
+        // Print Out The Value
+        System.out.println(friendsArray2);
+        System.out.println(friendsArrayList2);
+
+        // Array List Function
+        friendsArrayList2.contains("Chris"); // Contain Value? Return Boolean
+
+        // LOOP Through Each Array
+        // For Loop
+        for (int i = 0; i < friendsArrayList2.size(); i++) {
+            System.out.println(friendsArrayList2.get(i));
+        }
+
+        // Advanced For Loop
+        for (String name : friendsArrayList2) {
+            System.out.println(name);
+        }
+
+        // While Loop
+        int count = 0;
+        while (friendsArrayList2.size() > count) {
+            System.out.println(friendsArrayList2.get(count));
+            count++;
+        }
+
+        // Loop Using Iterator
+        Iterator iterator = friendsArrayList2.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+
     }
 }
